52a8ec4373b55b4ba6ee5dba6f9fdce5c993adb7
Rychard William <rychardwww1@gmail.com>
2024-04-25 16:55:32 -0300

    Commit

--- Converter/src/ConverçõesMoedas/MetodoConverção.java
+++ Converter/src/ConverçõesMoedas/MetodoConverção.java
@@ -7,9 +7,28 @@ public class MetodoConverção {
 	double converterDolar(double quantidade) {
 		return valor = quantidade / 5.165;
 	}
+	double dolarParaReal(double quantidade) {
+		return valor = quantidade * 5.165;
+	}
 
 	double converterEuro(double quantidade) {
 		return valor = quantidade / 5.541;
 	}
-
+	
+	double euroPesoArgentino (double quantidade) {
+		return valor = (quantidade * 5.541) / 939.441;
+	}
+	
+	double realPesoArgentino (double quantidade) {
+		return valor = quantidade * 169.492;
+	}
+	double pesoDolar (double quantidade) {
+		return valor = (quantidade * 0.8755 ) / 0.001;
+	}
+	
+	
+	
+	
+	
+	
 }
--- Converter/src/ConverçõesMoedas/Sistema.java
+++ Converter/src/ConverçõesMoedas/Sistema.java
@@ -6,19 +6,39 @@ public class Sistema {
 	public static void main(String[] args) {
 		MetodoConverção converter = new MetodoConverção();
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("Bem vindo ao conversor de moedas\n Do real para o Dolar digite => 'Dolar'\n Do real para o Dolar digite => 'Euro'");
+		System.out.println("Bem vindo/a ao conversor de moedas\n" 
+				+"****************************************************\n"
+				+ "Do real para o Dolar digite =>>> 'DOLAR'\n"
+				+ "Do Dolar para o Real digite =>>> 'DOLAR, REAL'\n" 
+				+ "Do real para o Euro digite =>>> 'EURO'\n"
+				+ "Do Euro para o Peso Argentino digite =>>> 'EURO, PESO'\n"
+				+ "Do Real para o Peso Argentino digite =>>> 'PESO'\n"
+				+ "Do Peso Argentino para o Dolar digite =>>> 'PESO, DOLAR'\n"
+				+ "****************************************************");
 		String opção = entrada.nextLine();
 		System.out.println("Qual o valor?");
-		double valor = Double.parseDouble(entrada.nextLine().replace(",", ".")) ; 
-		if(opção.equalsIgnoreCase("dolar")  ) {
+		double valor = Double.parseDouble(entrada.nextLine().replace(",", "."));
+		if (opção.equalsIgnoreCase("dolar")) {
 			converter.converterDolar(valor);
-			System.out.printf("O valor em Dolar é de %.3f", converter.valor);
-		}else if(opção.equalsIgnoreCase("euro")) {
+			System.out.printf("O valor %.3fR$ em Dolar é de %.3fUS", valor, converter.valor);
+		} else if (opção.equalsIgnoreCase("euro")) {
 			converter.converterEuro(valor);
-			System.out.printf("O valor em Euro é de %.3f", converter.valor);
+			System.out.printf("O valor %.3fR$ em Euro é de %.3fEUR", valor, converter.valor);
+		} else if (opção.equalsIgnoreCase("Dolar, Real")) {
+			converter.dolarParaReal(valor);
+			System.out.printf("O valor %.3fUS é de %.3fR$", valor, converter.valor);
+		} else if (opção.equalsIgnoreCase("'Euro, Peso")) {
+			converter.euroPesoArgentino(valor);
+			System.out.printf("O valor de %.3fEUR em Peso Argentino é de %.3fARS",valor,  converter.valor);
+		} else if (opção.equalsIgnoreCase("Peso")) {
+			converter.realPesoArgentino(valor);
+			System.out.printf("O valor de %.3fR$ em Peso Argentino é de %.3fARS", valor, converter.valor);
+		} else if (opção.equalsIgnoreCase("Peso, Dolar")) {
+			converter.pesoDolar(valor);
+			System.out.printf("O valor de %.3fUS em Peso Argentino é de %.3fARS", valor, converter.valor);
 		}
-	
 		
+
 		entrada.close();
 
 	}
